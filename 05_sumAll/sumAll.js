7fe512be13b0c8b9c48a76648dac1c26215df436
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2023-12-14 18:55:04 +0000

    Add solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(typeof(a) !== typeof(1) || typeof(b) !== typeof(1)){
+        return 'ERROR'
+    }
+    else if(b < a){
+        [a, b] = [b, a]
+    }
+    if ( a < 0){
+        return 'ERROR'
+    }
+    let ans = 0
+    for(let i = a; i <= b; i++){
+        ans += i
+    }
+    return ans
 };
 
 // Do not edit below this line
