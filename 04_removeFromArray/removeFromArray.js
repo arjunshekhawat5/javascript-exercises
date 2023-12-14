aab29ce53dcd3163860c331ba5c04f6410c81a4f
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2023-12-14 18:54:34 +0000

    Add solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
-
+    arr = arguments[0]
+    for (let i = 1; i < arguments.length; i++){
+        arr = arr.filter(n => n !== arguments[i])
+    }
+    return arr
 };
 
 // Do not edit below this line
