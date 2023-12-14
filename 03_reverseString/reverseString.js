3335925fd00ca4a94d5153a3ada6191e385a38c4
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2023-12-14 18:53:55 +0000

    Add solution for 02_r3_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    if (word.length < 1) return word
+    let ans = ''
+    for (let i = word.length - 1; i >= 0; i--){
+        ans += word[i]
+    }
 
+    return ans
 };
 
 // Do not edit below this line
